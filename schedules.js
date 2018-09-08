e0ed8d608b6f5ad5b335d023a220611cf22f7ba7
tsubasahonda <lenonystalicelight+tsubasahonda@gmail.com>
2018-09-08 17:13:06 +0900

    schedule.json吐き出すまで

--- schedules.js
+++ schedules.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 require('dotenv').config();
 
 class RankingScrapper {
@@ -16,27 +17,47 @@ class RankingScrapper {
       await page.goto(this.schedulesURL);
 
       let schedules = await page.evaluate(() => {
-        let dayIndex = [];
+        let dayIndex = {};
         let dailyIndex = [];
         let list = document.querySelectorAll('.table > tbody > tr');
-        let output
+        let day, hour, minutes, programs;
         for(let i=0; i < list.length; i++) {
+          //let hour, minutes, programs;
           if(list[i].classList.contains('text-white')) {
-            let text = list[i].querySelector('td:nth-child(1)').innerText
-            dayIndex.push({i, text});
+            day = list[i].querySelector('td:nth-child(1)').innerText;
           } else {
-            let hour = list[i].querySelector('td:nth-child(1)').innerText
-            let minutes = list[i].querySelectorAll('td:nth-child(2) > div > div:nth-child(1)');
-            let programs = list[i].querySelectorAll('td:nth-child(2) > div > div:nth-child(2)');
+            hour = list[i].querySelector('td:nth-child(1)').innerText;
+            minutes = list[i].querySelectorAll('td:nth-child(2) > div > div:nth-child(1)');
+            programs = list[i].querySelectorAll('td:nth-child(2) > div > div:nth-child(2)');
             let minute = {};
             for(let l=0; l < minutes.length; l++) {
-              let program = programs[l].querySelectorAll('span > a');
-              minute[minutes[l].innerText] = programs[l].innerText;
+              let program = programs[l].querySelectorAll(':scope > span > a');
+              let user = programs[l].querySelectorAll(':scope > span');
+              let programInfo = {}; 
+              for(let k=0; k < program.length; k++) {
+                try {
+                  programInfo[k] = {
+                    name: user[k].querySelector(':scope > span > a').innerText,
+                    program: program[k].innerText,
+                    programLink: program[k].getAttribute('href'),
+                    day: day,
+                    hour: hour,
+                    minutes: minutes[l].innerText
+                  };
+                } catch(e) {
+                  programInfo[k] = {
+                    name: '',
+                    program: program[k].innerText,
+                    programLink: program[k].getAttribute('href')
+                  };
+                }
+              }
+              minute[minutes[l].innerText] = {programInfo};
             }
             dailyIndex.push({i, hour, minute});
           }
         }
-        return {dayIndex, dailyIndex};
+        return dailyIndex;
       })
 
       await browser.close();
@@ -57,5 +78,7 @@ const Ranking = new RankingScrapper({
 
 Ranking.getRanking().then((data) => {
   //console.log(data);
-  console.log(data.dailyIndex);
+  console.log(data);
+  //console.log(data.dailyIndex[7].minute['0分'].programInfo);
+  fs.writeFile('schedules.json', JSON.stringify(data, null, ' '));
 });
